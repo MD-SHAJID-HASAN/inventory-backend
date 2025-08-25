3cd0364fb80930c7a6112b70cf0334a3db7cd22d
MD-SHAJID-HASAN <96662358+MD-SHAJID-HASAN@users.noreply.github.com>
2025-08-25 16:32:10 +0600

    Update app.js

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const app = express();
 
 
 app.use(cors({
-    origin: 'http://localhost:5173',  //frontend URL
+    origin: 'https://ledgerly-lilac.vercel.app',  //frontend URL
     credentials: true,
 }));
 
